4fc0b28c8b7dd9ab6988bdd62f97028820c3e29d
NICOLAS SCHNEIDER LAPOLLI GARCIA <nicolas_s_garcia@estudante.sesisenai.org.br>
2024-07-04 17:34:53 -0300

    abu2

--- index.js
+++ index.js
@@ -12,9 +12,40 @@ function FiltroPar(arr) {
 arr = [1,2,3,4,5,6]
 result = FiltroPar(arr)
 
+/* ------------------------------------------------------------------------------------ */
+
 const express = require('express')
 
+const { Client } = require('pg')
+
+require("dotenv").config()
+
+
+
+const client = new Client({
+
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+
+})
+
 const app = express()
+
+const connectDB = async () => {
+
+    client
+    .connect()
+    .then(() => {
+        console.log('a conexao funfo')
+    }) .catch((err) => {
+        console.error('erro nessa merda')
+    });
+
+};
+connectDB()
 
 app.get('/', function (req, res) {
     res.send('Nossa Api Funciona')
